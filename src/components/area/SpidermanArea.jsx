df60ec7245e4a8bb3a2987bde50cfd4a64e84c68
Vijul Malviya <vijulmalviya55@gmail.com>
2024-10-03 03:03:30 +0530

    Feature# add camera control for model

--- src/components/area/SpidermanArea.jsx
+++ src/components/area/SpidermanArea.jsx
@@ -1,13 +1,17 @@
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { SpiderMan } from "../../public/models/spiderman";
+import { CameraControl } from "../camera-setting/CameraControl";
 
 const SpiderManArea  = () => {
   return (
     <Canvas className="canvas" camera={{ position: [0, 0, 5.5] }}>
+      <CameraControl />
       <OrbitControls enableZoom={false} />
       <Environment preset="sunset" />
       <ambientLight intensity={0.8} color={"pink"} />
-      <boxGeometry/>
+      <boxGeometry />
+
     </Canvas>
   );
 };
