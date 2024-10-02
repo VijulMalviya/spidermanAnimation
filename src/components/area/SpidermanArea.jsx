3942b8caac4401a4a18e541a4248a822ffeb969d
Vijul Malviya <vijulmalviya55@gmail.com>
2024-10-03 03:45:39 +0530

    Feature# update model for responsivness and also update background Color

--- src/components/area/SpidermanArea.jsx
+++ src/components/area/SpidermanArea.jsx
@@ -5,15 +5,15 @@ import  Spiderman  from "../../models/spiderman/Spiderman";
 
 const SpiderManArea  = ({isAnimationPlayed}) => {
   return (
-    <Canvas className="canvas" style={{width: "100%", height: "100vh", backgroundColor: "darkRed"}} camera={{ position: [0, 0, 5.5] }}>
+    <Canvas className="canvas" style={{width: "100%", height: "100vh", background:"linear-gradient(135deg, #0d1b2a, #1b263b, #415a77, #778da9, #e63946)"}} camera={{ position: [0, 0, 5.5] }}>
       <CameraControl />
       <OrbitControls enableZoom={false} />
       <Environment preset="sunset" />
       <ambientLight intensity={0.8} color={"red"} />
       
       <Spiderman
-         scale={(7, 7, 7)}
-         position={[0, -7.95, -14]}
+         scale={window.innerWidth <= 768 ? [4, 4, 4] : [7, 7, 7]}
+         position={window.innerWidth <= 768 ? [0, -5, -10] : [0, -7.95, -14]}
          rotation={[0, 0, 0]}
          isAnimationPlayed={isAnimationPlayed}
       />
